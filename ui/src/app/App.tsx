76e9ff14318a52e9c83bd79a05793d06fdcd916a
Cause Chung <cuzfrog@139.com>
2019-01-06 16:18:39 +0800

    cender Console

--- ui/src/app/App.tsx
+++ ui/src/app/App.tsx
@@ -1,22 +1,15 @@
 import * as React from 'react';
 import './App.less';
-
-import logo from './logo.svg';
+import Console from '../console';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className='App'>
+                <Console/>
+            </div>
+        );
+    }
 }
 
 export default App;
