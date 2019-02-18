ce251c9f70422154aad6731e2cb84881a61f3bac
Cause Chung <cuzfrog@139.com>
2019-02-18 21:43:02 +0800

    use immer to simplify system reducer

--- ui/src/store/system/systemReducer.ts
+++ ui/src/store/system/systemReducer.ts
@@ -1,68 +1,49 @@
+import produce from 'immer';
 import {DefaultSystemState, GameType, SystemState, SystemStatus} from './systemState';
 import {nextNumEnum} from '../../utils';
 import {SystemAction} from './systemActions';
 import {ActionTypes} from '../actions';
 import {getGameKeyboard, MenuKeyboardLayout, PauseKeyboardLayout} from '../keyboardDef';
 
-const MAX_LEVEL = 9;
+const MAX_LEVEL = 8;
 const GameTypeValues: ReadonlyArray<number> = Object.keys(GameType).map(key => GameType[key]);
 
 export function systemReducer(state: SystemState = DefaultSystemState, action: SystemAction): SystemState {
-    switch (action.type) {
-        case ActionTypes.TOGGLE_PAUSE:
-            if (state.status === SystemStatus.IN_GAME) {
-                const inGamePaused = !state.inGamePaused;
-                return {
-                    ...state,
-                    inGamePaused,
-                    keyboardLayout: inGamePaused ? PauseKeyboardLayout : getGameKeyboard(state.gameType)
-                };
-            } else {
-                return state;
+    return produce(state, draft => {
+            switch (action.type) {
+                case ActionTypes.TOGGLE_PAUSE:
+                    if (state.status === SystemStatus.IN_GAME) {
+                        const inGamePaused = !state.inGamePaused;
+                        draft.inGamePaused = inGamePaused;
+                        draft.keyboardLayout = inGamePaused ? PauseKeyboardLayout : getGameKeyboard(state.gameType);
+                    }
+                    break;
+                case ActionTypes.ENTER_GAME:
+                    if (state.status === SystemStatus.MENU) {
+                        draft.status = SystemStatus.IN_GAME;
+                        draft.keyboardLayout = getGameKeyboard(state.gameType);
+                    }
+                    break;
+                case ActionTypes.EXIT_GAME:
+                    if (state.status === SystemStatus.IN_GAME) {
+                        draft.status = SystemStatus.MENU;
+                        draft.keyboardLayout = MenuKeyboardLayout;
+                    }
+                    break;
+                case ActionTypes.INCREASE_LEVEL:
+                    draft.level = state.level >= MAX_LEVEL ? 1 : state.level + 1;
+                    break;
+                case ActionTypes.DECREASE_LEVEL:
+                    draft.level = state.level <= 1 ? MAX_LEVEL : state.level - 1;
+                    break;
+                case ActionTypes.ADD_SCORE:
+                    draft.scores = state.scores.update(state.gameType, prevScore => prevScore + action.payload);
+                    break;
+                case ActionTypes.TOGGLE_GAME:
+                    const gameType = nextNumEnum(state.gameType, GameTypeValues);
+                    draft.gameType = gameType;
+                    draft.keyboardLayout = getGameKeyboard(gameType);
             }
-        case ActionTypes.ENTER_GAME:
-            if (state.status === SystemStatus.MENU) {
-                return {
-                    ...state,
-                    status: SystemStatus.IN_GAME,
-                    keyboardLayout: getGameKeyboard(state.gameType),
-                };
-            } else {
-                return state;
-            }
-        case ActionTypes.EXIT_GAME:
-            if (state.status === SystemStatus.IN_GAME) {
-                return {
-                    ...state,
-                    status: SystemStatus.MENU,
-                    keyboardLayout: MenuKeyboardLayout,
-                };
-            } else {
-                return state;
-            }
-        case ActionTypes.INCREASE_LEVEL:
-            return {
-                ...state,
-                level: state.level >= MAX_LEVEL ? 1 : state.level + 1
-            };
-        case ActionTypes.DECREASE_LEVEL:
-            return {
-                ...state,
-                level: state.level <= 1 ? MAX_LEVEL : state.level - 1
-            };
-        case ActionTypes.ADD_SCORE:
-            const scores = state.scores.update(state.gameType, prevScore => prevScore + action.payload);
-            return {
-                ...state, scores,
-            };
-        case ActionTypes.TOGGLE_GAME:
-            const gameType = nextNumEnum(state.gameType, GameTypeValues);
-            return {
-                ...state,
-                gameType,
-                keyboardLayout: getGameKeyboard(gameType)
-            };
-        default:
-            return state;
-    }
+        }
+    );
 }
