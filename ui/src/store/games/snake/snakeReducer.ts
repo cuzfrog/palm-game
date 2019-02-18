783f978be6512953a142ef29d623aeb403cc9509
Cause Chung <cuzfrog@139.com>
2019-02-18 22:07:24 +0800

    simplify snake reducer

--- ui/src/store/games/snake/snakeReducer.ts
+++ ui/src/store/games/snake/snakeReducer.ts
@@ -1,44 +1,39 @@
+import produce from 'immer';
+import {List} from 'immutable';
 import {DefaultSnakeGameState, SnakeGameState} from './snakeState';
 import {SnakeAction} from './snakeActions';
 import {ActionTypes} from '../../actions';
 import {isOppositeDirection, Point} from '../../types';
-import {List} from 'immutable';
 import {Specs} from '../../../Specs';
 import {randomInt} from '../../../utils';
 
 export function snakeGameReducer(state: SnakeGameState = DefaultSnakeGameState, action: SnakeAction): SnakeGameState {
-    switch (action.type) {
-        case ActionTypes.SET_DIRECTION:
-            if (isOppositeDirection(state.direction, action.payload) || state.direction === action.payload) {
-                return state;
-            } else {
-                return {
-                    ...state,
-                    direction: action.payload
-                };
-            }
-        case ActionTypes.SNAKE_HIT_WALL:
-        case ActionTypes.SNAKE_BITE_SELF:
-            if (state.life > 1) {
-                return {
-                    ...DefaultSnakeGameState,
-                    life: state.life - 1,
-                };
-            } else {
-                return DefaultSnakeGameState;
-            }
-        case ActionTypes.SNAKE_CREEP:
-            const {head, grown} = action.payload;
-            const length = state.body.size;
-            const body = state.body.toSeq().concat(head).takeLast(grown ? length + 1 : length).toList();
-            return {
-                ...state,
-                body,
-                bean: grown ? undefined : (state.bean ? state.bean : generateBean(body)),
-            };
-        default:
-            return state;
-    }
+    return produce(state, draft => {
+        switch (action.type) {
+            case ActionTypes.SET_DIRECTION:
+                if (!isOppositeDirection(state.direction, action.payload) && state.direction !== action.payload) {
+                    draft.direction = action.payload;
+                }
+                return;
+            case ActionTypes.SNAKE_HIT_WALL:
+            case ActionTypes.SNAKE_BITE_SELF:
+                if (state.life > 1) {
+                    return {
+                        ...DefaultSnakeGameState,
+                        life: state.life - 1,
+                    };
+                } else {
+                    return DefaultSnakeGameState;
+                }
+            case ActionTypes.SNAKE_CREEP:
+                const {head, grown} = action.payload;
+                const length = state.body.size;
+                const body = state.body.toSeq().concat(head).takeLast(grown ? length + 1 : length).toList();
+                draft.body = body;
+                draft.bean = grown ? undefined : (state.bean ? state.bean : generateBean(body));
+                return;
+        }
+    });
 }
 
 function generateBean(body: List<Point>): Point | undefined {
