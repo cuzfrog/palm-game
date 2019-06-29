c74483c8919f6bb38220d3aca3254e434674db50
Cause Chung <cuzfrog@139.com>
2019-06-29 23:44:58 +0800

    Set jest testMatch

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": fileMockPath,
         "\\.(css|less)$": "identity-obj-proxy"
     },
+    testMatch: ["<rootDir>/test/**/?(*.)+(spec|test).[tj]s?(x)"],
     testPathIgnorePatterns: ["/node_modules/", "/build/"],
     collectCoverage: false,
     collectCoverageFrom: [
