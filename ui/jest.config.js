44b23e97bf009f92778164ba793b66ab4c3f5e06
Cause Chung <cuzfrog@139.com>
2019-06-30 15:13:30 +0800

    Setup travis ci for ui build (#3)
    * Setup travis ci for ui build
    
    * Prefix rootDir to jest testPathIgnorePatterns
    
    * Add tslint to fork-ts-loader
    
    * Temporarily fix styledComponent type conflict
    
    * Limit ts auto import to node,jest; fix types conflict
    
    * Fail CI on build
    
    * Fix npm run build
    
    * Add license and ui-version badge
    
    * Setup codacy

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": fileMockPath,
         "\\.(css|less)$": "identity-obj-proxy"
     },
-    testPathIgnorePatterns: ["/node_modules/", "/build/"],
+    testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/build/"],
     collectCoverage: false,
     collectCoverageFrom: [
         "src/**/*.{ts,tsx,js,jsx}",
