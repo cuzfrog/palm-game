af10371559bc01c1dd5019ba3b4a30ac2e864030
Cause Chung <cuzfrog@139.com>
2019-07-02 12:07:49 +0800

    Remove LifeBar.less

--- ui/src/console/screen/LifeBar.tsx
+++ ui/src/console/screen/LifeBar.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
-import styles from './LifeBar.less';
+import styled from 'styled-components';
+import {ScreenColors} from './screenCss';
 
 interface Props {
     readonly hp: number;
     readonly maxHp: number;
     readonly count: number;
 }
+
+interface HeartProps {
+    readonly isActive: boolean;
+}
+
+const heartSize = 10;
+
+const LifeBarWrapper = styled.div`
+  overflow: auto;
+`;
+
+const Heart = styled.div`
+    display: inline-block;
+    margin: 2px;
+    position: relative;
+    width: ${heartSize}px;
+    height: ${heartSize}px;
+    float: left;
+    
+    &:before, &:after {
+      position: absolute;
+      content: "";
+      left: ${heartSize / 2}px;
+      top: 0;
+      width: ${heartSize / 2}px;
+      height: ${heartSize}px;
+      background: ${(props: HeartProps) => props.isActive ? ScreenColors.active : ScreenColors.deactivated};
+      border-radius: ${heartSize / 2} ${heartSize}/2 0 0;
+      transform: rotate(-45deg);
+      transform-origin: 0 100%;
+    }
+    &:after {
+      left: 0;
+      transform: rotate(45deg);
+      transform-origin: 100% 100%;
+    }
+`;
 
 export default class LifeBar extends React.PureComponent<Props, {}> {
     constructor(props: Readonly<Props>) {
@@ -20,9 +58,9 @@ export default class LifeBar extends React.PureComponent<Props, {}> {
 
     public render() {
         return (
-            <div className={styles.lifeBar}>
+            <LifeBarWrapper>
                 {renderHearts(this.props)}
-            </div>
+            </LifeBarWrapper>
         );
     }
 }
@@ -30,7 +68,5 @@ export default class LifeBar extends React.PureComponent<Props, {}> {
 // todo: optimize
 function renderHearts(props: Readonly<Props>) {
     const deActiveCnt = props.maxHp <= 0 ? props.count : (1 - props.hp / props.maxHp) * props.count;
-    return [...Array(props.count).keys()].map((i) =>
-        (<div className={i < deActiveCnt ? styles.heart : styles.activeHeart} key={i}/>)
-    );
+    return [...Array(props.count).keys()].map(i => (<Heart isActive={deActiveCnt <= i} key={i}/>));
 }
