f5106be4f773ccf06446e7ead18d26400812cb27
Cause Chung <cuzfrog@139.com>
2019-07-08 23:10:26 +0800

    Remove empty file

--- ui/src/console/keyboard/button.tsx
+++ ui/src/console/keyboard/button.tsx
@@ -6,62 +6,67 @@ import { mapStyledButton } from './button-styles';
 type BtnType = import('./button-styles').BtnType;
 
 interface Props {
-    type: BtnType;
-    caption?: string;
-    downHandler?: () => void;
-    upHandler?: () => void;
-    clickHandler?: () => void;
-    keyboardCode?: string;
+  type: BtnType;
+  caption?: string;
+  downHandler?: () => void;
+  upHandler?: () => void;
+  clickHandler?: () => void;
+  keyboardCode?: string;
+}
+
+interface State {
+  active: boolean;
 }
 
 const StyledButton = styled.div`
   ${(props: Props) => mapStyledButton(props.type)};
 `;
 
-export default class Button extends React.PureComponent<Props, {}> {
-    constructor(props: Props) {
-        super(props);
-        if (typeof props.downHandler !== typeof props.upHandler) {
-            throw ReferenceError('downHandler and upHandler must be both absent or both present.');
-        }
-        autoBind.react(this);
-        if (props.keyboardCode && props.clickHandler) {
-            handleKeyboardEvent(props.keyboardCode, props.clickHandler);
-        }
+export default class Button extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    if (typeof props.downHandler !== typeof props.upHandler) {
+      throw ReferenceError('downHandler and upHandler must be both absent or both present.');
     }
+    autoBind.react(this);
+    if (props.keyboardCode && props.clickHandler) {
+      handleKeyboardEvent(props.keyboardCode, props.clickHandler);
+    }
+    this.setState({ active: false }); // todo: use state to control css
+  }
 
-    public render() {
-        return (
-            <StyledButton
-                type={this.props.type}
-                onMouseDown={this.handleDown}
-                onMouseUp={this.handleUp}
-                onClick={this.props.clickHandler}
-            >
-                {this.props.caption}
-            </StyledButton>
-        );
-    }
+  public render() {
+    return (
+      <StyledButton
+        type={this.props.type}
+        onMouseDown={this.handleDown}
+        onMouseUp={this.handleUp}
+        onClick={this.props.clickHandler}
+      >
+        {this.props.caption}
+      </StyledButton>
+    );
+  }
 
-    private handleDown() {
-        if (this.props.downHandler) {
-            this.props.downHandler();
-            document.addEventListener('mouseup', this.handleUp);
-        }
+  private handleDown() {
+    if (this.props.downHandler) {
+      this.props.downHandler();
+      document.addEventListener('mouseup', this.handleUp);
     }
+  }
 
-    private handleUp() {
-        if (this.props.upHandler) {
-            document.removeEventListener('mouseup', this.handleUp);
-            this.props.upHandler();
-        }
+  private handleUp() {
+    if (this.props.upHandler) {
+      document.removeEventListener('mouseup', this.handleUp);
+      this.props.upHandler();
     }
+  }
 }
 
 function handleKeyboardEvent(code: string, keypressHandler: () => void) {
-    console.log('regiester keyboard event');
-    document.addEventListener('keypress', (ev: KeyboardEvent) => {
-        console.log('Capture keypress:' + ev.code);
-        if (ev.code === code) { keypressHandler(); }
-    });
+  console.log('regiester keyboard event');
+  document.addEventListener('keypress', (ev: KeyboardEvent) => {
+    console.log('Capture keypress:' + ev.code);
+    if (ev.code === code) { keypressHandler(); }
+  });
 }
