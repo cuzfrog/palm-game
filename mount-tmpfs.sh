710cdc9c19ad264b00b58cdd4613e642d5d9eaf6
Cause Chung <cuzfrog@139.com>
2019-08-07 20:26:43 +0800

    Add mount-tmpfs.sh

---
+++ mount-tmpfs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+mount -t tmpfs -o size=128M -o noatime tmpfs "$DIR/ui/build"
+mount -t tmpfs -o size=128M -o noatime tmpfs "$DIR/ui/coverage"
