acd91178595e208e44f7ad7d017e47d1648c8ec6
Cause Chung <cuzfrog@139.com>
2019-08-24 22:53:34 +0800

    Fix codacy warnings

--- ui/src/store/global-types.d.ts
+++ ui/src/store/global-types.d.ts
@@ -1,12 +1,12 @@
-type CoreAction = import('./action/core-actions').CoreAction;
-type KeyboardAction = import('./action/keyboard-actions').KeyboardAction;
-type SnakeAction = import('./action/snake-actions').SnakeAction;
-type TetrisAction = import('./action/tetris-actions').TetrisAction;
+type CoreAction = import("./action/core-actions").CoreAction;
+type KeyboardAction = import("./action/keyboard-actions").KeyboardAction;
+type SnakeAction = import("./action/snake-actions").SnakeAction;
+type TetrisAction = import("./action/tetris-actions").TetrisAction;
 type AppAction = CoreAction | SnakeAction | TetrisAction | KeyboardAction;
 
-type CoreState = import('./core/core-state').CoreState;
-type SnakeGameState = import('./games/snake-state').SnakeGameState;
-type TetrisGameState = import('./games/tetris-state').TetrisGameState;
+type CoreState = import("./core/core-state").CoreState;
+type SnakeGameState = import("./games/snake-state").SnakeGameState;
+type TetrisGameState = import("./games/tetris-state").TetrisGameState;
 interface AppState {
     readonly core: CoreState;
     readonly snake: SnakeGameState;
