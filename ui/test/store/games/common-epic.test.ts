dcf794ff0759e9aff15b71f26f1dd8d380d9be6d
Cause Chung <cuzfrog@139.com>
2019-08-24 22:39:08 +0800

    Fix common epic test

--- ui/test/store/games/common-epic.test.ts
+++ ui/test/store/games/common-epic.test.ts
@@ -1,5 +1,5 @@
 import { Lens } from "monocle-ts";
-import { GameStatus, GameType } from "src/domain";
+import { GameStatus } from "src/domain";
 import { CoreActions } from "src/store/core";
 import { heartbeatFunc } from "src/store/games/common-epic";
 import { defaultState, newTestScheduler } from "../support";
@@ -7,9 +7,10 @@ import { defaultState, newTestScheduler } from "../support";
 const pauseGameLens = Lens.fromPath<AppState>()(["core", "gameStatus"]);
 const BASIC_INTERVAL = 900;
 
-describe("creep epic", () => {
-  const mockCallback = jest.fn();
-  const epicFunc = heartbeatFunc(GameType.SNAKE, BASIC_INTERVAL, [], [], mockCallback);
+describe("common epic", () => {
+  const mockAction = CoreActions.dummy();
+  const mockCallback = jest.fn(a => mockAction);
+  const epicFunc = heartbeatFunc(defaultState.core.gameType, BASIC_INTERVAL, [], [], mockCallback);
 
   beforeEach(() => {
     mockCallback.mockClear();
@@ -18,13 +19,12 @@ describe("creep epic", () => {
   it("start and stop", () => {
     const state = pauseGameLens.set(GameStatus.RUNNING)(defaultState);
     newTestScheduler().run(({ hot, cold }) => {
-      const intervalMs = BASIC_INTERVAL - defaultState.core.getLevel() * 100;
+      const intervalMs = BASIC_INTERVAL - state.core.getLevel() * 100;
       const action$ = hot(`a ${intervalMs * 2}ms b`, { a: CoreActions.enterGame(), b: CoreActions.exitGame() });
       const state$ = cold("s", { s: state });
       const epic = epicFunc(action$, state$);
       epic.subscribe();
-      // expectObservable(epic)
-      //     .toBe('a 899ms a', {a: mockAction}); // todo try to fix mergeMap?
+      // expectObservable(epic).toBe("a 899ms a|", { a: mockAction }); // todo try to fix switchMap?
     });
     expect(mockCallback.mock.calls.length).toBe(2);
   });
